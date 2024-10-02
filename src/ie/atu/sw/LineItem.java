270b9073133d6fb8661870d27a129bcea84f7229
Joe1Jun <junker91joe@gmail.com>
2024-10-02 16:08:48 +0100

    Added the interface for Line items

---
+++ src/ie/atu/sw/LineItem.java
@@ -0,0 +1,17 @@
+package ie.atu.sw;
+
+public interface LineItem {
+
+	public void setItemNumber(String number);
+	public String getItemNumber();
+	
+	public void setItemName(String name);
+	public String getItemName();
+	
+	
+	public void setItemQuantity(int qty);
+	public int getItemQuantity();
+	
+	public void setItemPrice(float price);
+	public float getItemPrice();
+}
